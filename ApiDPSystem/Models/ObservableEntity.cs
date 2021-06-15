ac36d77376c561185fdc861b1e5e4212d0b0ad01
Blinov Vitaly <vblinov@htc-cs.ru>
2021-06-15 18:44:31 +0400

    Добавлено подтверждение email

---
+++ ApiDPSystem/Models/ObservableEntity.cs
@@ -0,0 +1,32 @@
+﻿using ApiDPSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDPSystem.Models
+{
+    class ConcreteObservable : IObservable
+    {
+        private List<IObserver> observers;
+        public ConcreteObservable()
+        {
+            observers = new List<IObserver>();
+        }
+        public void AddObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+                observer.Update();
+        }
+    }
+}
---
+++ ApiDPSystem/Models/ObserverEntity.cs
@@ -0,0 +1,16 @@
+﻿using ApiDPSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDPSystem.Models
+{
+    public class ObserverEntity : IObserver
+    {
+        public void Update()
+        {
+            // Добавить отправку сообщений
+        }
+    }
+}
